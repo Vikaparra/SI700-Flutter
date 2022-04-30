4506c78e3da703502122abd97430e14e325e133f
vikaparra <vikaparra@gmail.com>
2022-04-30 20:53:35 -0300

    DatePicker e TimePicker funcionando
    mas precisa ajustar ambos

--- Projeto/flutter_application_2/lib/view/testeCalendario.dart
+++ Projeto/flutter_application_2/lib/view/testeCalendario.dart
@@ -1,81 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/view/constants.dart';
 import 'package:flutter_application_2/components/rounded_button.dart';
-import 'package:flutter_application_2/view/login_screen.dart';
-import 'package:flutter_application_2/view/signup_screen.dart';
-
 import 'package:flutter/services.dart';
-// class Welcome extends StatelessWidget {
+
+//------------- TIMEPICKER -----------
+
+// Future mainCalend() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SystemChrome.setPreferredOrientations([
+//     DeviceOrientation.portraitUp,
+//     DeviceOrientation.portraitDown,
+//   ]);
+
+//   runApp(const TimePicker());
+// }
+
+// class TimePicker extends StatefulWidget {
+//   const TimePicker({Key? key}) : super(key: key);
+
 //   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         backgroundColor: kPrimaryColor,
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Image.asset(
-//                 'assets/images/logo.png',
-//                 width: 300,
-//               ),
-//               // ignore: prefer_const_constructors
-//             ],
-//           ),
-//         ));
+//   State<StatefulWidget> createState() {
+//     return TimePickerState();
 //   }
 // }
 
+// class TimePickerState extends State<TimePicker> {
+//   TimeOfDay? time;
 
-Future mainCalend() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+//   String getText() {
+//     if (time == null) {
+//       return 'Selecione o horário';
+//     } else {
+//       final hours = time?.hour.toString().padLeft(2, '0');
+//       final minutes = time?.minute.toString().padLeft(2, '0');
 
-  runApp(const TimePicker());
-}
+//       return '$hours:$minutes';
+//     }
+//   }
 
+//   @override
+//   Widget build(BuildContext context) => RoundedButton(
+//         text: getText(),
+//         press: () => pickTime(context),
+//       );
 
-class TimePicker extends StatefulWidget {
-  const TimePicker({Key? key}) : super(key: key);
+//   Future pickTime(BuildContext context) async {
+//     const initialTime = TimeOfDay(hour: 9, minute: 0);
+//     final newTime = await showTimePicker(
+//       context: context,
+//       initialTime: time ?? initialTime,
+//     );
+
+//     if (newTime == null) return;
+
+//     setState(() => time = newTime);
+//   }
+// }
+
+//--------------------------------------------------------------
+//------------------------- TIMEPICKER -------------------------
+
+class DatePickerWidget extends StatefulWidget {
+  const DatePickerWidget({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return TimePickerState();
+  _DatePickerWidgetState createState() {
+    return _DatePickerWidgetState();
   }
 }
 
-class TimePickerState extends State<TimePicker> {
-  TimeOfDay? time;
-  
-  String getText() {
-    if (time == null) {
-      return 'Select Time';
-    } else {
-      final hours = time?.hour.toString().padLeft(2, '0');
-      final minutes = time?.minute.toString().padLeft(2, '0');
+class _DatePickerWidgetState extends State<DatePickerWidget> {
+  DateTime? date;
 
-      return '$hours:$minutes';
+  String getText() {
+    if (date == null) {
+      return 'Selecione a data do evento';
+    } else {
+      return '${date?.day}/${date?.month}/${date?.year}';
     }
   }
 
   @override
   Widget build(BuildContext context) => RoundedButton(
         text: getText(),
-        press: () => pickTime(context),
+        press: () => pickDate(context),
       );
 
-  Future pickTime(BuildContext context) async {
-    const initialTime = TimeOfDay(hour: 9, minute: 0);
-    final newTime = await showTimePicker(
+  Future pickDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
       context: context,
-      initialTime: time ?? initialTime,
+      initialDate: date ?? initialDate,
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
     );
 
-    if (newTime == null) return;
+    if (newDate == null) return;
 
-    setState(() => time = newTime);
+    setState(() {
+      date = newDate;
+    });
   }
-  
 }

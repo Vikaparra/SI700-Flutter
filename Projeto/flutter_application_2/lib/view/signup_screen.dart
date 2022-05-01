9146635ca2d5b845e83bfcdf13b14f7596c79ef5
vikaparra <vikaparra@gmail.com>
2022-04-30 23:55:54 -0300

    corrigindo bug cadastro

--- Projeto/flutter_application_2/lib/view/signup_screen.dart
+++ Projeto/flutter_application_2/lib/view/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/components/rounded_button.dart';
+import 'package:flutter_application_2/view/date_time_picker.dart';
 import 'package:flutter_application_2/view/login_screen.dart';
 
 import 'constants.dart';
@@ -17,37 +19,37 @@ class TelaCadastroCuidador extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        height: MediaQuery.of(context).size.height * 1,
-        decoration: const BoxDecoration(
-          color: Color(0xff6a9cfd),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            tituloPaginaCadastro(),
-            Container(
-                decoration: const BoxDecoration(
-                    color: kWhiteColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40),
-                      bottomLeft: Radius.circular(0),
-                      bottomRight: Radius.circular(0),
-                    )),
-                child: const FormularioCad())
-          ],
-        ),
+    return Container(
+      height: MediaQuery.of(context).size.height * 1,
+      decoration: const BoxDecoration(
+        color: Color(0xff6a9cfd),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          tituloPaginaCadastro(context),
+          Container(
+              height: MediaQuery.of(context).size.height * 0.82,
+              decoration: const BoxDecoration(
+                  color: kWhiteColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                    bottomLeft: Radius.circular(0),
+                    bottomRight: Radius.circular(0),
+                  )),
+              child: const FormularioCad())
+        ],
       ),
     );
   }
 }
 
-Widget tituloPaginaCadastro() {
-  return const SizedBox(
-      width: double.infinity,
-      child: DefaultTextStyle(
+Widget tituloPaginaCadastro(context) {
+  return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: const DefaultTextStyle(
         style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.normal,
@@ -85,112 +87,100 @@ class FormularioState extends State<FormularioCad> {
               bottomLeft: Radius.circular(0),
               bottomRight: Radius.circular(0),
             )),
-        child: Column(
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration(labelText: 'Nome Completo'),
-                keyboardType: TextInputType.name,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration(labelText: 'Telefone'),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration(labelText: 'CPF'),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: TextField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                keyboardType: TextInputType.none,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 25.0),
-              child: TextField(
-                decoration: InputDecoration(labelText: 'Confirmar Senha'),
-                keyboardType: TextInputType.none,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 25.0),
-              child:
+        child: CustomScrollView(clipBehavior: Clip.hardEdge, slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Nome Completo'),
+                    keyboardType: TextInputType.name,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Telefone'),
+                    keyboardType: TextInputType.number,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'CPF'),
+                    keyboardType: TextInputType.number,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Senha'),
+                    keyboardType: TextInputType.none,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                    keyboardType: TextInputType.none,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                  ),
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                const Text(
-                  "Profissional",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                      fontFamily: "Comfortaa"),
+                    const Text(
+                      "Profissional",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                          fontFamily: "Comfortaa"),
+                    ),
+                    Checkbox(
+                        value: profissional,
+                        onChanged: (bool? inValue) {
+                          if (inValue != null) {
+                            setState(() {
+                              profissional = inValue;
+                            });
+                          }
+                        }),
+                    const Text(
+                      "Parente",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                          fontFamily: "Comfortaa"),
+                    ),
+                    Checkbox(
+                        value: parente,
+                        onChanged: (bool? inValue) {
+                          if (inValue != null) {
+                            setState(() {
+                              parente = inValue;
+                            });
+                          }
+                        })
+                  ]),
+                ]),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                RoundedButton(
+                  text: 'CADASTRAR',
+                  textColor: kWhiteColor,
+                  color: kSecondColor,
+                  press: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
                 ),
-                Checkbox(
-                    value: profissional,
-                    onChanged: (bool? inValue) {
-                      if (inValue != null) {
-                        setState(() {
-                          profissional = inValue;
-                        });
-                      }
-                    }),
-                const Text(
-                  "Parente",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                      fontFamily: "Comfortaa"),
-                ),
-                Checkbox(value: parente, onChanged: (bool? inValue) {
-                      if (inValue != null) {
-                        setState(() {
-                          parente = inValue;
-                        });
-                      }
-                    })
-              ]),
+              ],
             ),
-            ElevatedButton(
-              child: const Text(
-                'CADASTRAR',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.normal,
-                    fontFamily: "Comfortaa"),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Login()),
-                );
-              },
-            )
-          ],
-        ));
+          )
+        ]));
   }
 }

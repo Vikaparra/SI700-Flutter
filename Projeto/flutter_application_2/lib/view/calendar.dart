bcea114800da2f2a34281d68ff55fdc76e39b749
Myrelle Lopes <smyrellelopes@gmail.com>
2022-06-27 22:29:19 -0300

    tentando dar um sort na list

--- Projeto/flutter_application_2/lib/view/calendar.dart
+++ Projeto/flutter_application_2/lib/view/calendar.dart
@@ -65,25 +65,30 @@ class Calendar extends StatelessWidget {
   }
 
   Container getActListView(AppointCollection appointCollection) {
+    
     return Container(
         color: kSecondColor,
         child: ListView.builder(
             itemCount: appointCollection.length(),
             itemBuilder: (context, position) => Material(
-                    child: ListTile(
-                  isThreeLine: true,
-                  textColor: Colors.black,
-                  tileColor:
-                      tileColor(appointCollection.getNodeAtIndex(position)),
-                  leading: Icon(icons[position % icons.length]),
-                  trailing: GestureDetector(
-                      onTap: () {}, child: const Icon(Icons.delete)),
-
-                  title: Text(appointCollection.getNodeAtIndex(position).title),
-                  subtitle: Text(
-                      appointCollection.getNodeAtIndex(position).description+'\n'+appointCollection.getNodeAtIndex(position).date.toString(),
-                  // trailing: Text(appointCollection.getNodeAtIndex(position).date),
-                )))));
+                child: ListTile(
+                    isThreeLine: true,
+                    textColor: Colors.black,
+                    tileColor:
+                        tileColor(appointCollection.getNodeAtIndex(position)),
+                    leading: Icon(icons[position % icons.length]),
+                    trailing: GestureDetector(
+                        onTap: () {}, child: const Icon(Icons.delete)),
+                    title:
+                        Text(appointCollection.getNodeAtIndex(position).title),
+                    subtitle: Text(
+                      appointCollection.getNodeAtIndex(position).description +
+                          '\n' +
+                          appointCollection
+                              .getNodeAtIndex(position)
+                              .date
+                              .toString(),
+                    )))));
   }
 }
 
@@ -124,102 +129,3 @@ Widget title() {
         )),
   );
 }
-
-// class CustomListItem extends StatelessWidget {
-//   const CustomListItem({
-//     Key? key,
-//     required this.title,
-//     required this.user,
-//     required this.viewCount,
-//   }) : super(key: key);
-
-//   final String title;
-//   final String user;
-//   final int viewCount;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 5.0),
-//       child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: <Widget>[
-//           Expanded(
-//             flex: 3,
-//             child: _VideoDescription(
-//               title: title,
-//               user: user,
-//               viewCount: viewCount,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class _VideoDescription extends StatelessWidget {
-//   const _VideoDescription({
-//     Key? key,
-//     required this.title,
-//     required this.user,
-//     required this.viewCount,
-//   }) : super(key: key);
-
-//   final String title;
-//   final String user;
-//   final int viewCount;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: <Widget>[
-//           Text(
-//             title,
-//             style: const TextStyle(
-//               fontWeight: FontWeight.w500,
-//               fontSize: 14.0,
-//             ),
-//           ),
-//           const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
-//           Text(
-//             user,
-//             style: const TextStyle(fontSize: 10.0),
-//           ),
-//           const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
-//           Text(
-//             '$viewCount views',
-//             style: const TextStyle(fontSize: 10.0),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class MyStatelessWidget extends StatelessWidget {
-//   const MyStatelessWidget({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView(
-//       padding: const EdgeInsets.all(8.0),
-//       itemExtent: 106.0,
-//       children: const <CustomListItem>[
-//         CustomListItem(
-//           user: 'Flutter',
-//           viewCount: 999000,
-//           title: 'The Flutter YouTube Channel',
-//         ),
-//         CustomListItem(
-//           user: 'Dash',
-//           viewCount: 884000,
-//           title: 'Announcing Flutter 1.0',
-//         ),
-//       ],
-//     );
-//   }
-// }
